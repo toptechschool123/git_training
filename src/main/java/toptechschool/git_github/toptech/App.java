b716300858db82e8da8bb12173471d5adf6d1fc6
sayed <sayed.sadatibrahim@gmail.com>
2023-07-13 06:51:07 -0700

    first commit

---
+++ src/main/java/toptechschool/git_github/toptech/App.java
@@ -0,0 +1,13 @@
+package toptechschool.git_github.toptech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
